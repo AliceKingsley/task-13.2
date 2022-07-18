5e1f5d8564641e25b58978f4f35498836c42b8ee
Марина Максимова <m.maksimova@m-maksimova.local>
2022-07-18 23:09:46 +0300

    Задание 1 для 15 недели

--- index.js
+++ index.js
@@ -1,35 +1,85 @@
 let a = 0;
 let b = 0;
+let sign;
+
 const div = document.querySelector('.result');
+const input = document.querySelector('.number');
+const buttonSum = document.querySelector('.sum');
+const buttonDiff = document.querySelector('.diff');
+const buttonMultiple = document.querySelector('.multiple');
+const buttonDivision = document.querySelector('.division');
+const buttonResult = document.querySelector('.equals');
+const buttonClear = document.querySelector('.clear');
+
+input.addEventListener("change", askFirstNumber);
+buttonSum.addEventListener("click", calcSum);
+buttonDiff.addEventListener("click", calcDiff);
+buttonMultiple.addEventListener("click", calcMultipl);
+buttonDivision.addEventListener("click", calcDivision);
+buttonResult.addEventListener("click", equals);
+buttonClear.addEventListener("click", clear);
 
 function askFirstNumber() {
-    return Number(document.querySelector('.first-number').value);
+    a = Number(input.value);
+    input.removeEventListener("change", askFirstNumber);
+    return a;
 }
 
 function askSecondNumber() {
-    return Number(document.querySelector('.second-number').value);
+    b = Number(input.value);
+    input.removeEventListener("change", askSecondNumber);
+    div.classList.remove('error');
+    return b;
+}
+
+function clear() {
+    input.value = '';
+    a = 0;
+    b = 0;
+    sign = '';
+    input.placeholder = 'Введите первое число';
+    div.classList.remove('error');
+    div.textContent = '';
+    input.addEventListener("change", askFirstNumber);
+}
+
+function clearInput() {
+    input.value = '';
+    input.placeholder = 'Введите второе число';
+    input.addEventListener("change", askSecondNumber);
 }
 
 function calcSum() {
-    a = askFirstNumber();
-    b = askSecondNumber();
-    div.textContent = `Результат сложения: ${a + b}`;
+    sign = '+';
+    clearInput();
 }
 
 function calcDiff() {
-    a = askFirstNumber();
-    b = askSecondNumber();
-    div.textContent = `Результат вычитания: ${a - b}`;
+    sign = '-';
+    clearInput();
 }
 
 function calcMultipl() {
-    a = askFirstNumber();
-    b = askSecondNumber();
-    div.textContent = `Результат умножения: ${a * b}`;
+    sign = '*';
+    clearInput();
 }
 
 function calcDivision() {
-    a = askFirstNumber();
-    b = askSecondNumber();
-    div.textContent = `Результат деления: ${a / b}`;
+    sign = '/';
+    clearInput();
+}
+
+function equals() {
+    switch (sign) {
+        case '+': div.textContent = `Результат сложения: ${a + b}`; break;
+        case '-': div.textContent = `Результат вычитания: ${a - b}`; break;
+        case '*': div.textContent = `Результат умножения: ${a * b}`; break;
+        case '/': if (b != 0) {
+                            div.textContent = `Результат деления: ${a / b}`;
+                        } else {
+                            div.textContent = `На ноль делить нельзя!`;
+                            div.classList.add('error');
+                        }; break;
+        default: div.textContent = `Ты не ввёл данные`; div.classList.add('error');
+    }
 }
